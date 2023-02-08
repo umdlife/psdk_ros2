1d3e5b4ee40a6bcd37f72e0a4bf6dc170b9b733c
biancabnd <bianca@unmanned.life>
2023-02-08 16:53:14 +0100

    Add umd_psdk_wrapper package structure

---
+++ umd_psdk_wrapper/include/umd_psdk_wrapper/psdk_wrapper.hpp
@@ -0,0 +1,37 @@
+/* Copyright (C) 2023 Unmanned Life - All Rights Reserved
+ *
+ * This file is part of the `umd_psdk_wrapper` package and is subject to
+ * the terms and conditions defined in the file LICENSE.txt contained therein.
+ */
+/**
+ * @file psdk_wrapper.hpp
+ *
+ * @brief Header file for the psdk_wrapper class
+ *
+ * @author Bianca Bendris
+ * Contact: bianca@unmanned.life
+ *
+ */
+
+#ifndef UMD_PSDK_WRAPPER_INCLUDE_UMD_PSDK_WRAPPER_PSDK_WRAPPER_HPP_
+#define UMD_PSDK_WRAPPER_INCLUDE_UMD_PSDK_WRAPPER_PSDK_WRAPPER_HPP_
+
+namespace umd_psdk
+{
+    /**
+     * @class umd_psdk::PSDKWrapper
+     * @brief
+     */
+    class PSDKWrapper
+    {
+    public:
+        PSDKWrapper();
+        ~PSDKWrapper();
+
+    protected:
+    private:
+    };
+
+} // namespace umd_psdk
+
+#endif // UMD_PSDK_WRAPPER_INCLUDE_UMD_PSDK_WRAPPER_PSDK_WRAPPER_HPP_
