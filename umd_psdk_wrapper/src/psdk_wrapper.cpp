de37c0fda332d8766cf85e6c45c3a88c8474c74e
biancabnd <bianca@unmanned.life>
2023-03-10 17:00:07 +0100

    Add subscribers for RTK related topics

--- umd_psdk_wrapper/src/psdk_wrapper.cpp
+++ umd_psdk_wrapper/src/psdk_wrapper.cpp
@@ -540,23 +540,45 @@ PSDKWrapper::initialize_ros_elements()
   RCLCPP_INFO(get_logger(), "Initializing ROS publishers");
   attitude_pub_ = create_publisher<geometry_msgs::msg::QuaternionStamped>(
       "dji_psdk_ros/attitude", 10);
+  imu_pub_ = create_publisher<sensor_msgs::msg::Imu>("dji_psdk_ros/imu", 10);
+  velocity_ground_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(
+      "dji_psdk_ros/velocity_ground_ENU", 10);
+  position_fused_pub_ = create_publisher<umd_psdk_interfaces::msg::PositionFused>(
+      "dji_psdk_ros/position_fused", 10);
+  gps_fused_pub_ = create_publisher<umd_psdk_interfaces::msg::GPSFused>(
+      "dji_psdk_ros/gps_fused", 10);
+  gps_position_pub_ =
+      create_publisher<sensor_msgs::msg::NavSatFix>("dji_psdk_ros/gps_position", 10);
+  gps_velocity_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(
+      "dji_psdk_ros/gps_velocity", 10);
+  gps_details_pub_ = create_publisher<umd_psdk_interfaces::msg::GPSDetails>(
+      "dji_psdk_ros/gps_details", 10);
+  gps_signal_pub_ =
+      create_publisher<std_msgs::msg::UInt8>("dji_psdk_ros/gps_signal_level", 10);
+  gps_control_pub_ =
+      create_publisher<std_msgs::msg::UInt8>("dji_psdk_ros/gps_control_level", 10);
+  rtk_position_pub_ =
+      create_publisher<sensor_msgs::msg::NavSatFix>("dji_psdk_ros/rtk_position", 10);
+  rtk_velocity_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(
+      "dji_psdk_ros/rtk_velocity", 10);
+  rtk_yaw_pub_ =
+      create_publisher<umd_psdk_interfaces::msg::RTKYaw>("dji_psdk_ros/rtk_yaw", 10);
+  rtk_position_info_pub_ =
+      create_publisher<std_msgs::msg::UInt8>("dji_psdk_ros/rtk_position_info", 10);
+  rtk_yaw_info_pub_ =
+      create_publisher<std_msgs::msg::UInt8>("dji_psdk_ros/rtk_position_info", 10);
+
   acceleration_ground_pub_ = create_publisher<geometry_msgs::msg::AccelStamped>(
       "dji_psdk_ros/acceleration_ground", 10);
   acceleration_body_pub_ = create_publisher<geometry_msgs::msg::AccelStamped>(
       "dji_psdk_ros/acceleration_body", 10);
-  imu_pub_ = create_publisher<sensor_msgs::msg::Imu>("dji_psdk_ros/imu", 10);
-  velocity_ground_pub_ = create_publisher<geometry_msgs::msg::TwistStamped>(
-      "dji_psdk_ros/velocity_ground_ENU", 10);
   flight_status_pub_ =
       create_publisher<std_msgs::msg::UInt8>("dji_psdk_ros/fligh_status", 10);
   altitude_pub_ =
       create_publisher<umd_psdk_interfaces::msg::Altitude>("dji_psdk_ros/altitude", 10);
   relative_height_pub_ =
       create_publisher<std_msgs::msg::Float32>("dji_psdk_ros/relative_height", 10);
-  gps_position_pub_ =
-      create_publisher<sensor_msgs::msg::NavSatFix>("dji_psdk_ros/gps_position", 10);
-  rtk_position_pub_ =
-      create_publisher<sensor_msgs::msg::NavSatFix>("dji_psdk_ros/rtk_position", 10);
+
   magnetometer_pub_ = create_publisher<sensor_msgs::msg::MagneticField>(
       "dji_psdk_ros/magnetometer", 10);
   rc_pub_ = create_publisher<sensor_msgs::msg::Joy>("dji_psdk_ros/rc", 10);
@@ -570,8 +592,7 @@ PSDKWrapper::initialize_ros_elements()
       create_publisher<umd_psdk_interfaces::msg::Battery>("dji_psdk_ros/battery", 10);
   flight_anomaly_pub_ = create_publisher<umd_psdk_interfaces::msg::FlightAnomaly>(
       "dji_psdk_ros/flight_anomaly", 10);
-  position_fused_pub_ = create_publisher<umd_psdk_interfaces::msg::PositionFused>(
-      "dji_psdk_ros/position_fused", 10);
+
   relative_obstacle_info_pub_ =
       create_publisher<umd_psdk_interfaces::msg::RelativeObstacleInfo>(
           "dji_psdk_ros/relative_obstacle_info", 10);
@@ -584,15 +605,26 @@ PSDKWrapper::activate_ros_elements()
 {
   RCLCPP_INFO(get_logger(), "Activating ROS elements");
   attitude_pub_->on_activate();
-  acceleration_ground_pub_->on_activate();
-  acceleration_body_pub_->on_activate();
   imu_pub_->on_activate();
   velocity_ground_pub_->on_activate();
+  position_fused_pub_->on_activate();
+  gps_fused_pub_->on_activate();
+  gps_position_pub_->on_activate();
+  gps_velocity_pub_->on_activate();
+  gps_details_pub_->on_activate();
+  gps_signal_pub_->on_activate();
+  gps_control_pub_->on_activate();
+  rtk_position_pub_->on_activate();
+  rtk_velocity_pub_->on_activate();
+  rtk_yaw_pub_->on_activate();
+  rtk_position_info_pub_->on_activate();
+  rtk_yaw_info_pub_->on_activate();
+
+  acceleration_ground_pub_->on_activate();
+  acceleration_body_pub_->on_activate();
   flight_status_pub_->on_activate();
   altitude_pub_->on_activate();
   relative_height_pub_->on_activate();
-  gps_position_pub_->on_activate();
-  rtk_position_pub_->on_activate();
   magnetometer_pub_->on_activate();
   rc_pub_->on_activate();
   gimbal_angles_pub_->on_activate();
@@ -600,7 +632,6 @@ PSDKWrapper::activate_ros_elements()
   aircraft_status_pub_->on_activate();
   battery_pub_->on_activate();
   flight_anomaly_pub_->on_activate();
-  position_fused_pub_->on_activate();
   relative_obstacle_info_pub_->on_activate();
   home_position_pub_->on_activate();
 }
@@ -610,15 +641,26 @@ PSDKWrapper::deactivate_ros_elements()
 {
   RCLCPP_INFO(get_logger(), "Deactivating ROS elements");
   attitude_pub_->on_deactivate();
-  acceleration_ground_pub_->on_deactivate();
-  acceleration_body_pub_->on_deactivate();
   imu_pub_->on_deactivate();
   velocity_ground_pub_->on_deactivate();
+  position_fused_pub_->on_deactivate();
+  gps_fused_pub_->on_deactivate();
+  gps_position_pub_->on_deactivate();
+  gps_velocity_pub_->on_deactivate();
+  gps_details_pub_->on_deactivate();
+  gps_signal_pub_->on_deactivate();
+  gps_control_pub_->on_deactivate();
+  rtk_position_pub_->on_deactivate();
+  rtk_velocity_pub_->on_deactivate();
+  rtk_yaw_pub_->on_deactivate();
+  rtk_position_info_pub_->on_deactivate();
+  rtk_yaw_info_pub_->on_deactivate();
+
+  acceleration_ground_pub_->on_deactivate();
+  acceleration_body_pub_->on_deactivate();
   flight_status_pub_->on_deactivate();
   altitude_pub_->on_deactivate();
   relative_height_pub_->on_deactivate();
-  gps_position_pub_->on_deactivate();
-  rtk_position_pub_->on_deactivate();
   magnetometer_pub_->on_deactivate();
   rc_pub_->on_deactivate();
   gimbal_angles_pub_->on_deactivate();
@@ -626,7 +668,6 @@ PSDKWrapper::deactivate_ros_elements()
   aircraft_status_pub_->on_deactivate();
   battery_pub_->on_deactivate();
   flight_anomaly_pub_->on_deactivate();
-  position_fused_pub_->on_deactivate();
   relative_obstacle_info_pub_->on_deactivate();
   home_position_pub_->on_deactivate();
 }
@@ -636,15 +677,26 @@ PSDKWrapper::clean_ros_elements()
 {
   RCLCPP_INFO(get_logger(), "Cleaning ROS elements");
   attitude_pub_.reset();
-  acceleration_ground_pub_.reset();
-  acceleration_body_pub_.reset();
   imu_pub_.reset();
   velocity_ground_pub_.reset();
+  position_fused_pub_.reset();
+  gps_fused_pub_.reset();
+  gps_position_pub_.reset();
+  gps_velocity_pub_.reset();
+  gps_details_pub_.reset();
+  gps_signal_pub_.reset();
+  gps_control_pub_.reset();
+  rtk_position_pub_.reset();
+  rtk_velocity_pub_.reset();
+  rtk_yaw_pub_.reset();
+  rtk_position_info_pub_.reset();
+  rtk_yaw_info_pub_.reset();
+
+  acceleration_ground_pub_.reset();
+  acceleration_body_pub_.reset();
   flight_status_pub_.reset();
   altitude_pub_.reset();
   relative_height_pub_.reset();
-  gps_position_pub_.reset();
-  rtk_position_pub_.reset();
   magnetometer_pub_.reset();
   rc_pub_.reset();
   gimbal_angles_pub_.reset();
@@ -652,7 +704,6 @@ PSDKWrapper::clean_ros_elements()
   aircraft_status_pub_.reset();
   battery_pub_.reset();
   flight_anomaly_pub_.reset();
-  position_fused_pub_.reset();
   relative_obstacle_info_pub_.reset();
   home_position_pub_.reset();
 }
