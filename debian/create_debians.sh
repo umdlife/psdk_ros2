c8613e210796c01977e1e3e917331a3793411455
vicmassy <victor@unmanned.life>
2024-04-09 13:10:00 +0200

    Test workflow

--- debian/create_debians.sh
+++ debian/create_debians.sh
@@ -15,7 +15,7 @@ curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 
 apt update
-apt install -y --no-install-recommends python3-bloom python3-catkin-pkg dpkg-dev debhelper dh-python nlohmann-json3-dev python3-rosdep
+apt install -y --no-install-recommends python3-pip python3-bloom python3-catkin-pkg dpkg-dev debhelper dh-python nlohmann-json3-dev python3-rosdep
 rosdep init
 # cp psdk_ros2/debian/50-my-packages.list /etc/ros/rosdep/sources.list.d
 # cp psdk_ros2/debian/rosdep.yaml /
@@ -42,7 +42,7 @@ for PACKAGE in ${PACKAGE_LIST[@]}; do
 
     # We have to go to the ROS package parent directory
     cd $PACKAGE;
-    bloom-generate rosdebian --ros-distro humble --os-name ubuntu --os-version jammy
+    bloom-generate rosdebian --ros-distro humble
     debian/rules "binary --parallel --dpkg-shlibdeps-params=--ignore-missing-info"
 
     cd ..
